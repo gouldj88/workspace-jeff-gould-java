c670a5cae0fc41b3e5d89b317314f1074e23f7f8
Jeff Gould <gould.j88@gmail.com>
2021-02-09 15:03:18 -0500

    homework

--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/postgres/aggregate-functions-exercises.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/postgres/aggregate-functions-exercises.sql
@@ -1,4 +1,4 @@
-﻿-- The following queries utilize the "world" database.
+-- The following queries utilize the "world" database.
 -- Write queries for the following problems. 
 -- Notes:
 --   GNP is expressed in units of one million US Dollars
@@ -13,76 +13,185 @@
 -- The results should be ordered alphabetically by state name and then by city 
 -- name. 
 -- (19 rows)
+select name || ', ' || district as name_and_state, population
+from city
+where countrycode = 'USA'
+and district = 'Pennsylvania'
+ or district = 'West Virginia'
+ or district = 'Kentucky'
+ or district = 'Indiana'
+ or district = 'Michigan'
+ order by name, district
+;
 
 -- 2. The name, country code, and region of all countries in Africa.  The name and
 -- country code should be returned as a single column named country_and_code 
 -- and should contain values such as ‘Angola (AGO)’ 
 -- (58 rows)
-
+select name || ' ' || code as country_and_code, region
+from country
+where continent = 'Africa'
+;
 -- 3. The per capita GNP (i.e. GNP multipled by 1000000 then divided by population) of all countries in the 
 -- world sorted from highest to lowest. Recall: GNP is express in units of one million US Dollars 
 -- (highest per capita GNP in world: 37459.26)
+select (gnp * 1000000) / population
+from country
+where population > 0
+order by 1 desc;
 
 -- 4. The average life expectancy of countries in South America.
 -- (average life expectancy in South America: 70.9461)
+select avg(lifeexpectancy)
+from country
+where region = 'South America'
+;
 
 -- 5. The sum of the population of all cities in California.
 -- (total population of all cities in California: 16716706)
+select sum(population)
+from city
+where district = 'California';
 
 -- 6. The sum of the population of all cities in China.
 -- (total population of all cities in China: 175953614)
+select sum(population)
+from city
+where countrycode = 'CHN';
 
 -- 7. The maximum population of all countries in the world.
 -- (largest country population in world: 1277558000)
+select population
+from country
+order by population desc
+limit 1
+;
 
 -- 8. The maximum population of all cities in the world.
 -- (largest city population in world: 10500000)
+select population
+from city
+order by population desc
+limit 1;
 
 -- 9. The maximum population of all cities in Australia.
 -- (largest city population in Australia: 3276207)
+select population
+from city
+where countrycode = 'AUS'
+order by population desc
+limit 1;
 
 -- 10. The minimum population of all countries in the world.
 -- (smallest_country_population in world: 50)
+select population
+from country
+where population <> 0
+order by population asc
+limit 1;
+;
 
 -- 11. The average population of cities in the United States.
 -- (avgerage city population in USA: 286955.3795)
+select avg(population)
+from city
+where countrycode = 'USA'
+;
 
 -- 12. The average population of cities in China.
 -- (average city population in China: 484720.6997 approx.)
+select avg(population)
+from city
+where countrycode = 'CHN'
+;
 
 -- 13. The surface area of each continent ordered from highest to lowest.
 -- (largest continental surface area: 31881000, "Asia")
+select continent, sum(surfacearea) as totalsurface
+from country
+group by continent
+order by totalsurface desc
+;
 
 -- 14. The highest population density (population divided by surface area) of all 
 -- countries in the world. 
 -- (highest population density in world: 26277.7777)
+select population/surfacearea as density
+from country
+order by density desc
+limit 1;
 
 -- 15. The population density and life expectancy of the top ten countries with the 
 -- highest life expectancies in descending order. 
 -- (highest life expectancies in world: 83.5, 166.6666, "Andorra")
+select population/surfacearea as density, lifeexpectancy, name
+from country
+where lifeexpectancy is not null
+order by lifeexpectancy desc
+limit 10
+;
 
 -- 16. The difference between the previous and current GNP of all the countries in 
 -- the world ordered by the absolute value of the difference. Display both 
 -- difference and absolute difference.
 -- (smallest difference: 1.00, 1.00, "Ecuador")
+select ABS(gnp - gnpold) as Absdifference, gnp - gnpold as difference, name
+from country
+where gnpold is not null
+order by Absdifference
+;
+
+
 
 -- 17. The average population of cities in each country (hint: use city.countrycode)
 -- ordered from highest to lowest.
 -- (highest avg population: 4017733.0000, "SGP")
+select avg(population), countrycode
+from city
+group by countrycode
+order by avg desc
+;
 	
 -- 18. The count of cities in each state in the USA, ordered by state name.
 -- (45 rows)
+select district, count(*)
+from city
+where countrycode = 'USA'
+group by district
+order by district
+;
 	
 -- 19. The count of countries on each continent, ordered from highest to lowest.
 -- (highest count: 58, "Africa")
+select continent, count(*)
+from country
+group by continent
+order by count desc
+;
 	
 -- 20. The count of cities in each country ordered from highest to lowest.
 -- (largest number of  cities ib a country: 363, "CHN")
+select count(*), countrycode
+from city
+group by countrycode
+order by count desc
+;
 	
 -- 21. The population of the largest city in each country ordered from highest to 
 -- lowest.
 -- (largest city population in world: 10500000, "IND")
+select MAX(population), countrycode
+from city
+group by countrycode
+order by MAX desc
+;
 
 -- 22. The average, minimum, and maximum non-null life expectancy of each continent 
 -- ordered from lowest to highest average life expectancy. 
 -- (lowest average life expectancy: 52.5719, 37.2, 76.8, "Africa")
+select avg(lifeexpectancy), MIN(lifeexpectancy), MAX(lifeexpectancy), continent
+from country
+where lifeexpectancy is not null
+group by continent
+order by avg asc
+;
--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/lecture-code.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/lecture-code.sql
@@ -8,25 +8,71 @@
 --    ORDER BY ASC      -- Ascending Sequence (low-high)
 --    ORDER BY DESC     -- Descending Sequence (high-low)
 
--- Show Populations of all countries in acscending order
+-- There is no guarantee of the order of the rows in the result without an ORDER BY
+-- You can run the same query 1000 times without an ORDER BY, and get the same result
+-- The 1001st time might or might not be a different result
+--
+-- If the order of the rows in the result matter, code an ORDER BY
 
+-- In Standard SQL, the ORDER BY is always last in the query, 
+-- (postgreSQL) unless there is a LIMIT clause, which goes last
+
+
+
+-- Show Populations of all countries in acscending order
+select name, population
+from country
+order by population; -- if the order of the rows is important, code an ORDER BY
+ -- without an ORDER BY, the rows in the result are in whatever order the database gives it to you. It may not be consistent.
+ 
+select name, population
+from country
+order by population ASC; -- ASC is optional/default
 
 -- Show Populations of all countries in descending order
-
+select name, population
+from country
+order by population DESC;
 
 -- Show  the n ames of countries and continents in ascending order
+select name, continent
+from country
+order by continent desc, name;  -- to order by multiple columns, code column names separated by commas in the sequence you want them sorted. This will order continents in descending order, and name in ascending order.
+
+select name, continent
+from country
+order by continent, name desc; -- this will order continents in ascending order, and name in descending order.
 
 --------------------------------------------------------------------------------------------------------
 -- Limiting the number of rows in the result
 --
 -- LIMIT n   - Limit the number of rows in the result - always goes at thE end of the SELECT
 --
---
--- Show the name and average life expectancy of the countries with the 10 highest life expectancies.
+-- LIMIT is a postgreSQL extension to standard SQL (ANSI - American National Standard Institute, ISO - International Standards Organization)
 
+-- Other database managers limit rows cut call it something different
+-- You also have to code it somewhere else
+
+-- LIMIT is always the last in the SELECT
+
+-- Show the name and life expectancy of the countries with the 10 highest life expectancies.
+select name, lifeexpectancy
+from country
+where lifeexpectancy is not null
+order by lifeexpectancy desc -- show life expectancies from highest to lowest
+limit 10 -- only return the first 10 rows in the result
+;
+
+-- Show the name and life expectancy of the countries with the 10 lowest life expectancies.
+select name, lifeexpectancy
+from country
+where lifeexpectancy is not null
+order by lifeexpectancy -- show life expectancies from loswest to highest
+limit 10 -- only return the first 10 rows in the result
+;
 
 --------------------------------------------------------------------------------------------------------
--- Concatenating values 
+-- Concatenating string/character values 
 --
 -- the concat operator (||) may be used to concatenate character (string) values in a result
 --
@@ -34,12 +80,20 @@
 -- Show the name & state in the format: "city-name, state"
 -- of all cities in California, Oregon, or Washington.
 -- sorted by state then city
-
+select name || ', ' || district AS city_state
+  from city
+ where district = 'California' 
+    or district = 'Oregon'
+    or district = 'Washington'
+-- where district IN('California', 'Oregon', 'Washington');  <<--- can also code it this way
+order by district, city
+;
 
 
 --------------------------------------------------------------------------------------------------------
 -- Aggregate functions - produce one row in result for each group specified
---
+--                       rather than one row in the result for each row that matches the WHERE clause
+
 -- The group used by the aggregate functions is determined by the GROUP BY clause
 -- if no GROUP BY clause is specified, the group is the set of rows in the result
 --
@@ -47,9 +101,10 @@
 --     SUM(column-expression)   - arithmentic sum for group of a non-NULL values in expression 
 --     MIN(column-expression)   - lowest value for group of non-NULL values in expression 
 --     MAX(column-expression)   - highest value for group of non-NULL values in expression 
---     COUNT(*)                 - number of rows in the group
+--     COUNT(*)(row-expression) - number of rows in the group
 --     COUNT(column-expression) - number of rows for the group of non-NULL values in expression 
 --
+-- When you code a GROUP BY, the only columns allowed on the SELECT are aggregate functions and columns listed in the GROUP BY
 --
 -- AVG(), SUM() may only bes used with numeric data types
 -- MIN(), MAX() may be used with numeric, character, date, time datatypes
@@ -67,9 +122,18 @@
 
 
 -- Show The 10 largest countries (by surface area) in the world
-
+select name, surfacearea
+from country
+order by surfacearea desc
+limit 10
+;
 
 -- Show the number of countries who declared independence in 1991
+select count(*) as Number_Countries_Indep_Year_1991
+from country
+where indepyear = 1991
+group by name
+;
 
 --------------------------------------------------------------------------------------------------------
 -- GROUP BY  - Specify the group to which the aggregate functions apply
@@ -81,19 +145,46 @@
 --
 
 -- Show the number of countries where each language is spoken, order show them from most countries to least
+-- number of - count(*)
+-- each - group by
+select language, count(*)
+from countrylanguage
+group by language
+order by 2 desc;
 
 
 
 -- Show the average life expectancy of each continent ordered from highest to lowest
+select continent, avg(lifeexpectancy) -- one row in the result with the average life expectancy across all countries
+from country
+where lifeexpectancy is not null
+group by continent
+order by 2 desc
 
+;
 
+-- Show average life expectancy in the world for each continent
+select continent, avg(lifeexpectancy) as Avg_Life_Expectancy-- one row in the result for each continent the average life expectancy for that continent
+  from country
+  group by continent -- specify the groups we want for the aggregate function
+;
+
+-- Show the total population in Ohio
+select sum(population) as Total_People_In_Ohio
+from city
+where district = 'Ohio'
+;
 
 -- Exclude Antarctica from consideration for average life expectancy
 
 
 
 -- What is the sum of the population of cities in each state in the USA ordered by state name
-
+select district, avg(population) as population
+from city
+where countrycode = 'USA'
+group by district
+order by district;
 
 
 -- What is the average population of cities in each state in the USA ordered by state name
