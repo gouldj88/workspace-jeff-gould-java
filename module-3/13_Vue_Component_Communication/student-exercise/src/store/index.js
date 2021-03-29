e3c2cde56561d6f372861f89293a974e48a3e64f
Jeff Gould <gould.j88@gmail.com>
2021-03-29 15:48:50 -0400

    message

--- module-3/13_Vue_Component_Communication/student-exercise/src/store/index.js
+++ module-3/13_Vue_Component_Communication/student-exercise/src/store/index.js
@@ -4,9 +4,43 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    books: [
+      {
+      title: "Kafka on the Shore",
+      author: "Haruki Murakami",
+      read: false,
+      isbn: "9781400079278"
+      },
+      {
+      title: "The Girl With All the Gifts",
+      author: "M.R. Carey",
+      read: true,
+      isbn: "9780356500157"
+      },
+      {
+      title: "The Old Man and the Sea",
+      author: "Ernest Hemingway",
+      read: true,
+      isbn: "9780684830490"
+      },
+      {
+      title: "Le Petit Prince",
+      author: "Antoine de Saint-Exupéry",
+      read: false,
+      isbn: "9783125971400"
+      }
+     ]  
+  },
+  mutations: {
+    FLIP_READ_STATE(state, bookToChange) {
+      bookToChange.read = ! bookToChange.read;
+    },
+    ADD_BOOK(state, book) {      // Add a review to our data array - receives the state and the new review
+      state.books.unshift(book); // Takes the new review passed and add it to the start of the array
+    }
+  },
   actions: {},
   modules: {},
-  strict: true
+  strict: true, 
 });
