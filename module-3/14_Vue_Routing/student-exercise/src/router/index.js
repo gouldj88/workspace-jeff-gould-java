9dce046a9347d52595ed84a7b562322f6b88c6ce
Jeff Gould <gould.j88@gmail.com>
2021-04-01 09:01:37 -0400

    message

--- module-3/14_Vue_Routing/student-exercise/src/router/index.js
+++ module-3/14_Vue_Routing/student-exercise/src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Books from '@/views/Home.vue';
+import MyBooks from '@/views/MyBooks.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'books',
+    component: Books
+  },
+  {
+    path: `/myBooks`,
+    name: `my-books`,
+    component: MyBooks
+  }
 ];
 
 const router = new VueRouter({
