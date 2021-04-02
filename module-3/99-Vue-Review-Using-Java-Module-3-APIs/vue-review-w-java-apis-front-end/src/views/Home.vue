5d33a803be9427e993e93db0ab0fd2f2c37441bd
Frank Fella <frank@techelevator.com>
2021-04-02 06:05:05 -0400

    Instructor Push 04/02/2021 06:05:04

---
+++ module-3/99-Vue-Review-Using-Java-Module-3-APIs/vue-review-w-java-apis-front-end/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/squirrel-hotel-2.jpg" />
+    <Home />
+  </div>
+</template>
+
+<script>
+               // @ is an alias to /src
+import Home from "@/components/Home.vue";
+
+export default {
+  name: "home",
+  components: {
+    Home
+  }
+};
+</script>
